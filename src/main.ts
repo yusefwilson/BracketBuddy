45fe4af74e8daa8d42651c2c9b1aadda3c917026
Yusef Wilson <yusefwilsonx@gmail.com>
2024-12-18 18:41:16 +1300

    Add preload and renderer scripts
    How does this work with react?

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { app, BrowserWindow } from 'electron';
+import * as path from 'node:path';
 
 // so that the app reloads when the code changes in development
 app.isPackaged || require('electron-reloader')(module);
 
 const create_window = async () => {
 
+    console.log('preload path: ' + path.join(__dirname, 'preload.js'))
     const window = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     });
 
     await window.loadFile('../public/index.html');
