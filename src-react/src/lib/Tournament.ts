a0cc714467316ac59bcca406da03ed9507d7c2c8
Yusef Wilson <yusefwilsonx@gmail.com>
2025-02-20 13:44:01 -0500

    Update Tournament.ts

--- src-react/src/lib/Tournament.ts
+++ src-react/src/lib/Tournament.ts
@@ -11,6 +11,23 @@ class Tournament {
         this.date = date;
     }
 
+    addBracket(bracket: Bracket) {
+        if (!this.brackets) {
+            this.brackets = [];
+        }
+        this.brackets.push(bracket);
+    }
+
+    save() {
+        // save to database
+
+    }
+
+    static load(filename: string): Tournament {
+        // load from database
+        return new Tournament('Tournament', new Date());
+    }
+
 }
 
 export default Tournament;
