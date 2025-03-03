605020e65905aaf56d21b699297e9b8fadfd77bd
Yusef Wilson <yusefwilsonx@gmail.com>
2025-03-03 13:53:46 -0500

    (consider part of last commit

--- src-react/src/pages/Home.tsx
+++ src-react/src/pages/Home.tsx
@@ -5,6 +5,7 @@ import { CURRENT_STATE } from '../components/App';
 import TournamentInfoCard from '../components/TournamentInfoCard';
 
 import Tournament from '../lib/Tournament';
+import TournamentInputModal from '../components/TournamentInputModal';
 
 export default function Home() {
 
@@ -12,6 +13,8 @@ export default function Home() {
   const navigate = useNavigate();
 
   const [allTournaments, setAllTournaments] = useState<Tournament[]>();
+  const [tournamentModalOpen, setTournamentModalOpen] = useState(false);
+
 
   // load saved tournaments
   useEffect(() => {
@@ -19,15 +22,18 @@ export default function Home() {
       const tournaments = await Tournament.loadAllTournaments();
       setAllTournaments(tournaments);
     }
-  }, []);
+
+    loadTournaments();
+  }, [tournamentModalOpen]);
 
   return (
     <div className='bg-slate-800 flex flex-col text-white text-center items-center gap-4'>
       <h1>Welcome to BracketBuddy</h1>
       <p>BracketBuddy is a tournament management system that helps you organize and run your tournaments.</p>
-      <button className='bg-yellow-500 p-4 rounded-md flex-shrink'>Create Tournament</button>
+      <button className='bg-yellow-500 p-4 rounded-md flex-shrink' onClick={() => { setTournamentModalOpen(true); }}>Create Tournament</button>
       <h1>My Tournaments:</h1>
       {allTournaments?.map((tournament, index) => <TournamentInfoCard key={index} tournament={tournament} onClick={() => { state?.setTournament(tournament); navigate('/tournament') }} />)}
+      {tournamentModalOpen && <TournamentInputModal setTournamentModalOpen={setTournamentModalOpen} />}
     </div>
   );
 }
