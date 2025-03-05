b7734480a6eb65d5a04d877ae9b9841802ff763b
Yusef Wilson <yusefwilsonx@gmail.com>
2025-03-05 14:18:02 -0500

    Move Remove button on TournamentInfoCard

--- src-react/src/components/TournamentInfoCard.tsx
+++ src-react/src/components/TournamentInfoCard.tsx
@@ -3,19 +3,18 @@ import Tournament from '../lib/Tournament'
 export default function TournamentInfoCard({ tournament, onClick, onRemoveClick }: { tournament: Tournament, onClick: () => void, onRemoveClick: () => void }) {
 
     return (
-        <div className='bg-slate-600 p-2 rounded-md gap-2 relative' onClick={onClick}>
-
-            {/* Delete Button (X) */}
-            <button className='absolute top-2 right-2 bg-red-500 text-white px-2 py-1 rounded-md' onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-                e.stopPropagation(); // Prevents the click from reaching the parent div
-                console.log('Delete button clicked and event propagation stopped');
-                onRemoveClick();
-            }}>X</button>
-
+        <div className='bg-slate-600 p-2 rounded-md gap-2s' onClick={onClick}>
             {/* Tournament Info */}
             <h1>{'Name: ' + tournament.name.toString()}</h1>
             <h1>{'Date: ' + tournament.date.toLocaleDateString('en-US')}</h1>
             <h1>{'Number of classes: ' + tournament.brackets.length.toString()}</h1>
+
+            {/* Delete Button (X) */}
+            <button className='bg-red-500 text-white px-2 py-1 rounded-md' onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+                e.stopPropagation(); // Prevents the click from reaching the parent div
+                console.log('Delete button clicked and event propagation stopped');
+                onRemoveClick();
+            }}>Remove</button>
         </div>
     )
 }
