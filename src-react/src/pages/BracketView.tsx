84b8c6b60e241b269c4833b26b98ccf314d224b3
Yusef Wilson <yusefwilsonx@gmail.com>
2025-06-05 22:28:43 -0400

    Style winners nicely
    I think there is a bug on adding a large amount of competitors, somewhere around 20 or 21. Need to fix

--- src-react/src/pages/BracketView.tsx
+++ src-react/src/pages/BracketView.tsx
@@ -42,50 +42,95 @@ export default function BracketView() {
     }
   }, [competitorNames]);
 
-  const HORIZONTAL_GAP = 300;
-  const HORIZONTAL_OFFSET = 0;
-  const VERTICAL_GAP = 150;
-  const VERTICAL_OFFSET = 50;
-  const EXTRA_VERTICAL_OFFSET = 25
+  const HORIZONTAL_GAP = 200;
+  const INITIAL_VERTICAL_GAP = 100;
+  const EXTRA_VERTICAL_OFFSET = 25;
+
+  const WINNER_HORIZONTAL_OFFSET = 25;
+  const WINNER_VERTICAL_OFFSET = 25;
 
   // given information about a round, calculate where on the screen the match should be placed.
-  const calculateWinnerPosition = (roundIndex: number, matchIndex: number, roundType: RoundType,): number[] => {
+  const calculateInitialWinnerPosition = (roundIndex: number, matchIndex: number, roundType: RoundType,): number[] => {
 
-    let x = roundIndex * HORIZONTAL_GAP + HORIZONTAL_OFFSET;
-    let y = matchIndex * VERTICAL_GAP + VERTICAL_OFFSET + (roundType === 'initialWinner' ? EXTRA_VERTICAL_OFFSET : 0);
+    let x = roundIndex * HORIZONTAL_GAP + WINNER_HORIZONTAL_OFFSET;
+    let y = matchIndex * INITIAL_VERTICAL_GAP + WINNER_VERTICAL_OFFSET + (roundType === 'initialWinner' ? EXTRA_VERTICAL_OFFSET : 0);
     return [x, y];
   }
 
-  // [roundIndex, matchIndex, roundType, match][]
-  const allWinnerMatches = [];
-
-  for (let roundIndex = 0; roundIndex < (bracket?.winnersBracket.length || 0); roundIndex++) {
-    for (let matchIndex = 0; matchIndex < (bracket?.winnersBracket[roundIndex].matches.length || 0); matchIndex++) {
-
-      let roundType = 'winner' as RoundType;
-      if (!isPowerOfTwo(competitorNames.length) && roundIndex === 0) { roundType = 'initialWinner'; } // common special case for non-power of two brackets
-      console.log('calculated roundType to be: ', roundType);
-      let match = bracket?.winnersBracket[roundIndex].matches[matchIndex];
-
-      allWinnerMatches.push([roundIndex, matchIndex, roundType, match]);
-    }
+  const calculateWinnerPosition = (roundIndex: number, parentMatch0Height: number, parentMatch1Height: number): number[] => {
+    let x = roundIndex * HORIZONTAL_GAP + WINNER_HORIZONTAL_OFFSET;
+    let y = (parentMatch0Height + parentMatch1Height) / 2;
+    return [x, y];
   }
 
-  // layout pass
-  useLayoutEffect(() => {
-  });
+  // {match, x, y}[][]
+  const winnerMatches = [];
+
+  // calculate initial rounds positions (if power of 2, only one round, if not power of 2, two rounds)
+  if (!isPowerOfTwo(competitorNames.length)) {
+    winnerMatches.push(bracket?.winnersBracket[0].matches.map((match, index) => {
+      let [x, y] = calculateInitialWinnerPosition(0, index, 'initialWinner');
+      return { match, x, y };
+    }));
+    winnerMatches.push(bracket?.winnersBracket[1].matches.map((match, index) => {
+      let [x, y] = calculateInitialWinnerPosition(1, index, 'winner');
+      return { match, x, y };
+    }));
+  }
+  else {
+    winnerMatches.push(bracket?.winnersBracket[0].matches.map((match, index) => {
+      let [x, y] = calculateInitialWinnerPosition(0, index, 'winner');
+      return { match, x, y };
+    }));
+  }
+
+  // iteratively calculate the positions of the rest of the matches by referencing and taking averages of the heights of their parents
+  for (let roundIndex = isPowerOfTwo(competitorNames.length) ? 1 : 2; roundIndex < (bracket?.winnersBracket.length || 0); roundIndex++) {
+    const previousRoundMatches = winnerMatches[roundIndex - 1];
+
+    if (!previousRoundMatches || previousRoundMatches.length === 0) {
+      console.warn(`No matches found for round index ${roundIndex - 1}`);
+      continue;
+    }
+
+    winnerMatches.push(bracket?.winnersBracket[roundIndex].matches.map((match, index) => {
+
+      // edge case when there is one parent
+      if (previousRoundMatches.length === 1) {
+        const parentMatch = previousRoundMatches[0];
+        const [x, y] = calculateWinnerPosition(roundIndex, parentMatch.y, parentMatch.y);
+        return { match, x, y };
+      }
+
+
+      // find parent matches using winnerMatches last round
+      const parentMatch0 = previousRoundMatches[index * 2];
+      const parentMatch1 = previousRoundMatches[index * 2 + 1];
+
+      if (!parentMatch0 || !parentMatch1) {
+        console.warn(`Parent matches not found for round index ${roundIndex} and match index ${index}`);
+        return { match, x: 0, y: 0 }; // fallback
+      }
+
+      const [x, y] = calculateWinnerPosition(roundIndex, parentMatch0.y, parentMatch1.y);
+      return { match, x, y };
+    }));
+  }
+
+  // flatten the winnerMatches array
+  const flattenedWinnerMatches = winnerMatches.flat();
 
   console.log('about to render bracket ', bracket);
 
   return (
-    <div className='rounded-md bg-orange-400 p-2 flex flex-row gap-4 h-full'>
+    <div className='rounded-md bg-orange-400 p-2 flex flex-row gap-4 h-full max-h-[92%]'>
 
-      <div className='bg-pink-500 flex flex-col p-2'>
+      <div className='bg-gray-400 flex flex-col p-2 rounded-md'>
         <CompetitorInput competitors={competitorNames} setCompetitors={setCompetitorNames} />
       </div>
 
       {/* <Bracket display */}
-      <div className='bg-blue-400 p-2 rounded-md flex flex-col gap-4 h-full w-full relative'>
+      <div className='bg-gray-400 rounded-md flex flex-col gap-4 h-full w-full relative overflow-auto'>
 
         {/* Y-level guide lines */}
         {[50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900].map((y, idx) => (
@@ -98,10 +143,10 @@ export default function BracketView() {
           </div>
         ))}
 
-        {allWinnerMatches.map(element => {
-          let [x, y] = calculateWinnerPosition(element[0] as number, element[1] as number, element[2] as RoundType);
+        {flattenedWinnerMatches.map(element => {
+          let { match, x, y } = element as { match: Match, x: number, y: number };
           console.log('about to render MATCHVIEW with x: ', x, ' and y: ', y);
-          return <MatchView match={element[3] as Match} updateMatch={updateMatch} x={x} y={y} />
+          return <MatchView match={match} updateMatch={updateMatch} x={x} y={y} />
         })}
 
       </div>
