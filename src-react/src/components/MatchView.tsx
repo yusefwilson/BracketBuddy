8eb955ccb470b42c768ffdcdd7d735d1dbf00f73
Yusef Wilson <yusefwilsonx@gmail.com>
2025-06-15 15:24:45 -0400

    Make MatchView divs clickable as well as checkboxes

--- src-react/src/components/MatchView.tsx
+++ src-react/src/components/MatchView.tsx
@@ -18,13 +18,13 @@ export default function MatchView({ match, updateMatch, x, y }:
       {/* Match container */}
       <div className='rounded-md bg-transparent p-2 flex flex-col gap-2'>
         {/* {<h3 className='text-center font-bold'>Match {match.id}</h3>} */}
-        <div className='flex flex-row justify-between items-center p-2 rounded-md bg-blue-400 w-44'>
+        <div className='flex flex-row justify-between items-center p-2 rounded-md bg-blue-400 w-44' onClick={() => toggleWinner(0)}>
           <div className='text-sm truncate w-0 flex-1'>
             {match.competitor0Name || `[Enter Name]`}
           </div>
           <WinnerCheckbox toggleWinner={() => toggleWinner(0)} checked={match.winner === 0} />
         </div>
-        <div className='flex flex-row justify-between items-center p-2 rounded-md bg-blue-400 w-44'>
+        <div className='flex flex-row justify-between items-center p-2 rounded-md bg-blue-400 w-44' onClick={() => toggleWinner(1)}>
           <div className='text-sm truncate w-0 flex-1'>
             {match.competitor1Name || `[Enter Name]`}
           </div>
--- src-react/src/components/WinnerCheckbox.tsx
+++ src-react/src/components/WinnerCheckbox.tsx
@@ -1,6 +1,14 @@
 export default function WinnerCheckbox({ toggleWinner, checked }: { toggleWinner: () => void, checked: boolean }) {
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation();
+    toggleWinner();
+  }
+  const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    e.stopPropagation();
+  };
+
   return (
-    <input className='bg-green-400 p-4 rounded-md' type='checkbox' onChange={toggleWinner} checked={checked}></input>
+    <input className='bg-green-400 p-4 rounded-md' type='checkbox' onChange={handleChange} checked={checked} onClick={handleClick} />
   )
 }
