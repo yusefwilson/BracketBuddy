26ef42059f0a9538f252d5fee01e27c30cdc2b4d
Yusef Wilson <yusefwilsonx@gmail.com>
2025-08-16 23:58:42 -0400

    Try to fix imports (and fuck it up)

--- src-react/vite.config.ts
+++ src-react/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './', // required for electron, makes paths relative to the root of the project
+  resolve: {
+    alias: {
+      "@shared": path.resolve(__dirname, "../src-shared"),
+      "@electron": path.resolve(__dirname, "../src-electron")
+    }
+  }
 })
--- src-shared/utils.ts
+++ src-shared/utils.ts
@@ -9,18 +9,6 @@ function greatestPowerOf2LessThanOrEqualTo(n: number): number {
 
 function isPowerOfTwo(n: number) { return n > 0 && (n & (n - 1)) === 0; };
 
-const getSaveData = async (): Promise<Record<string, any>> => {
-    const data = await window.electron.getSaveData();
-    return data;
-}
-
-const saveKeyValue = async (key: string, value: any): Promise<void> => {
-    await window.electron.saveKeyValue(key, value);
-}
-
-
-
 export {
-    greatestPowerOf2LessThanOrEqualTo, isPowerOfTwo,
-    getSaveData, saveKeyValue
+    greatestPowerOf2LessThanOrEqualTo, isPowerOfTwo
 };
