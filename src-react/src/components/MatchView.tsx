d6c7931b9900fbf2354fd9eaeb03c66b611a6b68
Yusef Wilson <yusefwilsonx@gmail.com>
2025-11-02 23:39:32 -0500

    Remove unused import

--- src-react/src/components/MatchView.tsx
+++ src-react/src/components/MatchView.tsx
@@ -1,4 +1,4 @@
-import { ExclamationCircleIcon, ExclamationTriangleIcon } from '@heroicons/react/24/outline';
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 import { MatchDTO } from '../../../src-shared/MatchDTO';
 import { MatchStatus } from '../../../src-shared/types';
 
