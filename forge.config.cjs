ef4eb23b26c082b32a533320c991f4f7b59ba2b9
Yusef Wilson <yusefwilsonx@gmail.com>
2025-11-25 23:04:23 -0500

    Update forge.config.cjs

--- forge.config.cjs
+++ forge.config.cjs
@@ -29,7 +29,7 @@ module.exports = {
         exe: "BracketBuddy.exe",                 // exe filename
         setupExe: "BracketBuddy-Setup.exe",      // setup filename
         setupIcon: "./assets/icon.ico",          // icon for installer & exe
-        iconUrl: "https://yusefwilson.github.io/BracketBuddy/assets/icon.ico", // hacky kinda way to get free icon hosting from github
+        iconUrl: "https://raw.githubusercontent.com/yusefwilson/BracketBuddy/refs/heads/release/assets/icon.ico", // hacky kinda way to get free icon hosting from github
         loadingGif: "./assets/loading.gif",      // optional loading animation for installer
         noMsi: true,                             // disables generating MSI, since Squirrel handles it
       }
