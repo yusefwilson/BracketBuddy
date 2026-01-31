1c4f69aecb0156797779b54d04b7114712535147
Yusef Wilson <yusefwilsonx@gmail.com>
2026-01-30 20:28:54 -0500

    Update forge.config.cjs

--- forge.config.cjs
+++ forge.config.cjs
@@ -5,14 +5,14 @@ module.exports = {
   packagerConfig: {
     asar: true,
     extraResource: ["./build-react", "./assets"],
-    x64ArchFiles: [
-      'Contents/Resources/app.asar.unpacked/src-react/node_modules/@rollup/rollup-darwin-arm64/rollup.darwin-arm64.node'
-    ],
     icon: "./assets/icon",
     executableName: "BracketBuddy", // <-- ensures the exe is BracketBuddy.exe
     appBundleId: "com.yusefwilson.bracketbuddy",
     osxUniversal: {
       mergeASARs: true,
+      x64ArchFiles: [
+      'app.asar.unpacked/src-react/node_modules/@rollup/rollup-darwin-arm64/rollup.darwin-arm64.node'
+    ]
     },
     osxSign: {
       identity: "Developer ID Application",
